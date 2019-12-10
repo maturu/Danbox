c8fe31b4e6e1d21b9910e213995ea4c94750efb0
shinamon-channel <m.shinamon@gmail.com>
2019-12-11 01:54:18 +0900

    feat: edit user-icon in profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     root_path
@@ -6,5 +7,15 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     new_session_path(resource_name)
+  end
+
+  def after_update_path_for(resource)
+    account_page_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :avatar_cache, :remove_avatar])
   end
 end
