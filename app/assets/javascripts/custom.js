79f4b684f6758e96d5f6a7b2487f2eac5630f604
shinamon-channel <m.shinamon@gmail.com>
2019-12-15 00:47:03 +0900

    feat: add caption post feature and display it on modal window

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -4,7 +4,9 @@ $(document).on('turbolinks:load', function() {
       var reader = new FileReader();
       reader.onload = function(e) {
         $("#post-prev").css('display', 'block');
-        $("#post-img-prev").css('display', 'initial');
+        $("#post-caption").css('display', 'inline');
+        $("#post-caption-count").css('display', 'inline');
+        $("#post-img-prev").css('display', 'inline');
         $("#post-img-prev").attr('src', e.target.result);
       }
       reader.readAsDataURL(input.files[0]);
@@ -15,11 +17,35 @@ $(document).on('turbolinks:load', function() {
   });
 
   $("#post-toggle").on('click', function() {
-    $(".post-form").toggle(function() {
+    $(".post-form").slideToggle(function() {
       $("#post-prev").css('display', 'none');
       $("#post-img-prev").css('display', 'none');
+      $("#post-caption").css('display', 'none');
+      $("#post-caption-count").css('display', 'none');
       $("#post-img-prev").attr('src', "#");
       $("#post-img").val('');
     });
+    $(".post-form").toggleClass("active");
   });
+
+  $("#post-caption").keyup(function() {
+    var count = $(this).val().length;
+    $("#post-caption-count").text(count+"/140");
+    if(count == 0){ $("#post-caption-count").text("0/140"); }
+  });
+
+  $(".post-content").click(function() {
+    $("#modal-window").css('display', 'flex');
+    $("body").css('overflow', 'hidden');
+    $("#modal-window .modal-item-action").css('display', 'inline');
+    $("#modal-window").html($(this).prop('outerHTML'));
+    $("#modal-window").fadeIn(200);
+  });
+});
+
+$(document).click(function(event) {
+  if(!$(event.target).closest('.modal-item').length) {
+    $("body").css('overflow', 'scroll');
+    $("#modal-window").fadeOut(200);
+  }
 });
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,26 +2,23 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:index, :show, :create, :destroy]
 
   def index
+    @users = User.all
     @posts = Post.all.order('id DESC')
     @post = Post.new
   end
 
   def show
-    @posts = Post.where(user_id: current_user.id).order('id DESC')
+    @user = User.find(params[:id])
   end
 
   def create
-    if !!post_params
-      @post = Post.new(post_params)
-      @post.user_id = current_user.id
-      if @post.save
-        redirect_back(fallback_location: root_path)
-      else
-        redirect_back(fallback_location: root_path) 
-      end
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_back(fallback_location: root_path)
     else
-      flash[:alert] = "投稿する画像を選択してください"
-      redirect_to root_path
+      flash[:alert] = params[:post][:caption].length > 140 ? "見出しは140文字以内で入力してくだい" : "投稿する画像を選択してください"
+      redirect_back(fallback_location: root_path) 
     end
   end
 
@@ -35,6 +32,6 @@ class PostsController < ApplicationController
 
   protected
     def post_params
-      params.require(:post).permit(:content, :content_cache, :remove_content) if !!params[:post]
+      params.require(:post).permit(:content, :content_cache, :remove_content, :caption)
     end
 end
