e5a065368f159478e6d409508a7b2f6eaef46cdb
maturu <maturu.410@gmail.com>
2019-12-20 17:06:55 +0900

    fix: gemfile

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,8 +261,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  #config.omniauth :twitter, ENV['API_KEY'], ENV['API_Secret']
-  config.omniauth :twitter, "9Dxd6g9NZuQGSMQwUeZUqcbYk", "4pKbxClxUflDcOADR9JYJ01o8kTGgGQRmAxLFaNs1hls1WiJOv"
+  config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
