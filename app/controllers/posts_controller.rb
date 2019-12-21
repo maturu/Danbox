6af7454675ea1e8a695efba15b29ef8a90326fde
maturu <maturu.410@gmail.com>
2019-12-21 11:41:26 +0900

    fix: tweet feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,8 +50,9 @@ class PostsController < ApplicationController
       @client = Twitter::REST::Client.new do |config|
         config.consumer_key = ENV['TWITTER_API_KEY']
         config.consumer_secret = ENV['TWITTER_API_SECRET']
-        config.access_token = ENV['TWITTER_ACCESS_TOKEN']
-        config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+        user_auth = current_user.authentications.first
+        config.access_token = user_auth.token
+        config.access_token_secret = user_auth.secret
       end
     end
 end
