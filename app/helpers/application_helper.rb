701a354eaf547d6c2760df454e63042373313cd9
maturu <maturu.410@gmail.com>
2019-12-22 15:28:31 +0900

    feat: add light seo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
       reverse: true,
       charset: 'utf-8',
       description: 'ダンボールを使った工作やアートなどのあらゆる作品を画像投稿機能を使って共有し、ダンボールの限界に挑むための新しいSNSです。',
-      keywords: 'Danbox, danbox, Cardboard, Cardboard Art, Craft, ダンボックス, ダンボール工作, ダンボールアート, 工作',
+      keywords: 'Danbox, danbox, Cardboard, Cardboard Art, Craft, ダンボックス, ダンボール工作, ダンボールアート, 工作, danbox sns, ダンボール sns, 工作 sns, craft sns, ダンボール webサービス',
       canonical: request.original_url,
       separator: '|',
       icon: [
